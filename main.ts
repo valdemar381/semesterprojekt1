b8ea00804ce8eb9571e3d38ef4e266113c35d6bb
Valdemar Rønn Cross <kontakt@crosscompany.dk>
2024-12-06 13:26:58 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,44 @@ function DefineRooms (RoomToDefine: number[], XCol: number, XRow: number, StartC
         }
     }
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tørretumbler`, function (sprite, location) {
+    if (GameIsAcive) {
+        tileUtil.loadConnectedMap(MapConnectionKind.Door1)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`ovn`, function (sprite, location) {
+    if (GameIsAcive) {
+        tileUtil.loadConnectedMap(MapConnectionKind.Door1)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`vaskemaskine`, function (sprite, location) {
+    if (GameIsAcive) {
+        tileUtil.loadConnectedMap(MapConnectionKind.Door1)
+    }
+})
 function SetFunctions () {
+    MainCharacter = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    MainTileMap = tilemap`level`
+    MenuTileMap = tilemap`MenuTilemap`
+    tiles.setCurrentTilemap(MainTileMap)
+    tileUtil.connectMaps(MenuTileMap, MainTileMap, MapConnectionKind.Door1)
     Stue = []
     Soveværelse = []
     Badeværelse = []
@@ -14,38 +51,34 @@ function SetFunctions () {
     Vaskerum = []
     Gang = []
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`føntørrer`, function (sprite, location) {
+    if (GameIsAcive) {
+        tileUtil.loadConnectedMap(MapConnectionKind.Door1)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`køleskab 2`, function (sprite, location) {
+    if (GameIsAcive) {
+        tileUtil.loadConnectedMap(MapConnectionKind.Door1)
+    }
+})
+let MenuTileMap: tiles.TileMapData = null
+let MainTileMap: tiles.TileMapData = null
+let GameIsAcive = false
 let Gang: number[] = []
 let Vaskerum: number[] = []
 let Køkken: number[] = []
 let Badeværelse: number[] = []
 let Soveværelse: number[] = []
 let Stue: number[] = []
-tiles.setCurrentTilemap(tilemap`level`)
-let mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(mySprite, 100, 100)
-scene.cameraFollowSprite(mySprite)
-tiles.placeOnRandomTile(mySprite, assets.tile`myTile0`)
+let MainCharacter: Sprite = null
 SetFunctions()
+controller.moveSprite(MainCharacter, 100, 100)
+tiles.placeOnTile(MainCharacter, tiles.getTileLocation(13, 20))
+scene.cameraFollowSprite(MainCharacter)
 DefineRooms(Stue, 8, 11, 17, 7)
 DefineRooms(Soveværelse, 7, 6, 3, 7)
 DefineRooms(Badeværelse, 7, 4, 3, 14)
 DefineRooms(Køkken, 13, 4, 3, 2)
 DefineRooms(Vaskerum, 8, 4, 17, 2)
 DefineRooms(Gang, 5, 11, 11, 7)
+GameIsAcive = true
